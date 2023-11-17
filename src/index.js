09c468b3e4ac4cc1ec0b00add667480f7297c03b
GustavoExequielAybar <aybargustavoe@gmail.com>
2023-11-17 17:10:36 -0300

    add folder, files and configure

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+
+const app = express();
+app.set('port', process.env.PORT || 4001)
+app.listen(app.get('port'), () => {
+    console.log('=================================');
+    console.log('servidor escuchando en puerto '+ app.get('port'));
+    console.log('=================================');
+})
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public/index.html')));
+
+app.get('/',(req,res)=>{
+    res.send('Esto es una prueba de un get desde mi backend')
+})
