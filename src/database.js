2714bed4c79d95e3f89a979783482bafcaa356a6
GustavoExequielAybar <aybargustavoe@gmail.com>
2023-12-02 07:57:53 -0300

    config file .env

--- src/database.js
+++ src/database.js
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 import "dotenv/config"
 const URI = process.env.MONGODB_URL;
 mongoose.connect(URI);
-// const url = 'mongodb://127.0.0.1:27017/ef_rc_2023_backend'
-// mongoose.connect(url);
 const connection = mongoose.connection;
 
 connection.once('open', () => {
